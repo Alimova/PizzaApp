a7086e47b29be188d4a2b5537c1a558b4be0e80b
Anna Alimova <new@alimov.net>
2017-04-15 11:59:13 +0300

    small tweaks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import {RouterModule, Routes} from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 import { AngularFireModule, AuthProviders, AuthMethods } from 'angularfire2';
 import {FirebaseService} from './services/firebase.service';
 
