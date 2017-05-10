8b26b10e1b2aa79a26a485def32c6c71459b93f2
Anna Alimova <new@alimov.net>
2017-05-11 00:11:00 +0300

    heroku deployment

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
 /**
  * Created by alimov on 5/10/17.
  */
+// server.js
+const express = require('express');
+const app = express();
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+// Start the app by listening on the default
+// Heroku port
+app.listen(process.env.PORT || 8080);
